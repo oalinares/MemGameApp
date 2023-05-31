1ab8e855b6cdfded13e90243ce5029e6fcbc4ee8
unknown <oalinares.jr@gmail.com>
2023-05-31 15:39:48 -0500

    3rd commit, added memory cards, altered margins, and included clicklistener

--- app/src/main/java/com/oalinaresjr/memorygame/MemoryBoardAdapter.kt
+++ app/src/main/java/com/oalinaresjr/memorygame/MemoryBoardAdapter.kt
@@ -1,23 +1,51 @@
 package com.oalinaresjr.memorygame
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
 
 class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
-    RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder() {
+    RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+    companion object {
+        private const val MARGIN_SIZE = 10
+        private const val TAG = "MemoryBoardAdapter"
     }
 
+    //onCreateViewHolder is responsible for figuring out how to create one view of our RecyclerView
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cardWidth = parent.width / 2 - (2 * MARGIN_SIZE)
+        val cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
+        val cardSideLength = min(cardWidth, cardHeight)
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as MarginLayoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+        return ViewHolder(view)
+    }
+    //getItemCount is how many elements are in our RecyclerView
     override fun getItemCount() = numPieces
 
+    /* onBindViewHolder is responsible for taking the data which is at this position and binding it to the passed
+    ViewHolder*/
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(position
+        )
     }
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+        fun bind(position: Int) {
+            imageButton.setOnClickListener {
+                Log.i(TAG, "Clicked on position $position")
+            }
+        }
+    }
 }
